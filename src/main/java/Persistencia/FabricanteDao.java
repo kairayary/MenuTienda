cd58c07a1e23b40943464320934e3f84969dc12a
kairayary <kaira.abreu@gmail.com>
2023-04-19 17:28:00 -0300

    Creación de Dao y FabricanteDao en Persistencia

--- src/main/java/Persistencia/FabricanteDao.java
+++ src/main/java/Persistencia/FabricanteDao.java
@@ -1,4 +1,35 @@
 package Persistencia;
 
-public class FabricanteDao {
+import Entidad.Fabricante;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FabricanteDao extends DAO{
+    public List<Fabricante> ObtenerFabricante() throws Exception{
+        try{
+            String sql = "Select * from fabricante;";
+
+            ConsultarBD(sql);
+
+            List<Fabricante>fabricantes = new ArrayList<>();
+            Fabricante fabricante;
+            while (resultset.next()) {
+                fabricante = new Fabricante();
+                fabricante.setCodigo(resultset.getInt(1));
+                fabricante.setNombre(resultset.getString(2));
+
+                fabricantes.add(fabricante);
+
+            }
+            return fabricante;
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+
+            throw new Exception("Ha ocurrido un error al obtener fabricante!!!");
+
+        }finally {
+            DesconectarBD();
+        }
+    }
 }
