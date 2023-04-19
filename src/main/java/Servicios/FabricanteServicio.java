c48b9f8ff53351e38982bd5cf5a58168c9a66c52
cecilia.mndiaz@gmail.com <cecilia.mndiaz@gmail.com>
2023-04-19 18:06:13 -0300

    servicio

--- src/main/java/Servicios/FabricanteServicio.java
+++ src/main/java/Servicios/FabricanteServicio.java
@@ -1,4 +1,26 @@
 package Servicios;
 
 public class FabricanteServicio {
+    private FabricanteDAO fabricanteDAO = new FabricanteDAO();
+
+    public void verFabricantes() throws Exception{
+
+        try {
+            List<Fabricante> fabricante = fabricanteDAO.TenerFabricante();
+            if (fabricante.isEmpty()){
+                throw new Exception("No hay registros de fabrincantes");
+            }else{
+                System.out.println("Lista de fabricantes");
+                System.out.printf("%-15s%-40\n", "CODIGO", "Nombre");
+                for(Fabricante fabricante : fabricantes){
+                    System.out.printf("%-15s%-40s\n", fabricante.getCodigo(), fabricante.getNombre());
+
+                }
+            }
+        }catch (Exception e){
+            throw e;
+
+        }
+
+    }
 }
